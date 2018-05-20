21974988e07f07ded042362ee49cb3e6190b4722
Colin PIREYRE <cpireyre@student.42.fr>
2018-05-20 07:48:01 +0200

    added struct s_options to store specs

--- parse/parse.h
+++ parse/parse.h
@@ -6,7 +6,7 @@
 /*   By: cpireyre <cpireyre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/17 10:57:36 by cpireyre          #+#    #+#             */
-/*   Updated: 2018/05/19 07:52:03 by cpireyre         ###   ########.fr       */
+/*   Updated: 2018/05/19 08:30:30 by cpireyre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,14 @@
 
 # include "libft.h"
 
-t_byte		get_options(const char *spec);
+typedef struct		s_options
+{
+	t_byte		flags;
+	int			field_width;
+	int			precision;
+	t_byte		length_mod;
+}					t_options;
+
+t_byte				get_options(const char *spec);
 
 #endif
