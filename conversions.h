d2dcadb94b35ba3f93dbdda5fd4a22d6358bcb36
Colin PIREYRE <cpireyre@student.42.fr>
2018-06-09 12:55:18 +0200

    norm-approved comments. asinine

--- conversions.h
+++ conversions.h
@@ -6,7 +6,7 @@
 /*   By: cpireyre <cpireyre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/09 11:27:21 by cpireyre          #+#    #+#             */
-/*   Updated: 2018/06/09 11:56:19 by cpireyre         ###   ########.fr       */
+/*   Updated: 2018/06/09 12:53:57 by cpireyre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,27 +16,48 @@
 # include "parse.h"
 # include "wchar.h"
 
-/* d + i, c. int conversions. */
+/*
+** d + i, c. int conversions.
+*/
+
 char	*signed_dec(int arg, t_options options);
 t_byte	u_char(int arg, t_options options);
 
-/* o, u, x + X. unsigned int conversions. */
+/*
+ ** o, u, x + X. unsigned int conversions.
+*/
+
 char	*u_octal(t_uint arg, t_options options);
 char	*u_dec(t_uint arg, t_options options);
-char	*u_hex(t_uint arg, t_options options, t_bool case);
+char	*u_hex(t_uint arg, t_options options, t_bool upcase);
 
-/* D, O, U. long int conversions. */
+/*
+** D, O, U. long int conversions.
+** I will use these to handle ld, lo, lu, etc. by casting
+** arg to long int and calling the following functions. Maybe.
+*/
+
 char	*l_signed_dec(long int arg, t_options options);
 char	*l_u_octal(long int arg, t_options options);
-char	*l_u_dec(long int arg, t_options options, t_bool case);
+char	*l_u_dec(long int arg, t_options options, t_bool upcase);
 
-/* s. const char * conversion. */
+/*
+**  s. const char * conversion.
+*/
+
 char	*string(const char *arg, t_options options);
 
-/* C, S. wint_t and wchar_t * conversions. */
+/*
+** C, S. wint_t and wchar_t * conversions.
+*/
+
 char	*mb_char(wint_t arg, t_options options);
 char	*mb_string(const wchar_t *arg, t_options options);
 
-/* p. pointer conversion. similar to %#x. */
+/*
+** p. pointer conversion. similar to %#x.
+*/
+
 char	*ptr_addr(void *arg, t_options options);
+
 #endif
