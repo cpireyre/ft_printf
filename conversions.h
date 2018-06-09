cbf75b1fe8d6dd5e64b8458977681dd5975dbe28
Colin PIREYRE <cpireyre@student.42.fr>
2018-06-09 11:48:30 +0200

    added prototypes of conversion functoins. first pass. perhaps they should all be void and write directly to bufer.

---
+++ conversions.h
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   conversions.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cpireyre <cpireyre@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/06/09 11:27:21 by cpireyre          #+#    #+#             */
+/*   Updated: 2018/06/09 11:47:51 by cpireyre         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CONVERSIONS_H
+# define CONVERSIONS_H
+
+# include "parse.h"
+# include "wchar.h"
+
+/* d + i, c. int conversions. */
+char	*signed_dec(int arg, t_options options);
+t_byte	u_char(int arg, t_options options);
+
+/* o, u, x + X. unsigned int conversions. */
+char	*octal(t_uint arg, t_options options);
+char	*u_dec(t_uint arg, t_options options);
+char	*u_hex(t_uint arg, t_options options, t_bool case);
+
+/* s. const char * conversion. */
+char	*string(const char *arg, t_options options);
+
+/* C, S. wint_t and wchar_t * conversions. */
+char	*mb_char(wint_t arg, t_options options);
+char	*mb_string(const wchar_t *arg, t_options options);
+
+/* p. pointer conversion. similar to %#x. */
+char	*ptr_addr(void *arg, t_options options);
+#endif
