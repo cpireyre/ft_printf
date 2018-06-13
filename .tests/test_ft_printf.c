5204324bf1b48cdd8fc97c3d6ca22a5fd0ea7fee
Colin PIREYRE <cpireyre@student.42.fr>
2018-06-13 12:57:10 +0200

    tests pertaining to %d %i and flags

--- .tests/test_ft_printf.c
+++ .tests/test_ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: cpireyre <cpireyre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/12 09:06:25 by cpireyre          #+#    #+#             */
-/*   Updated: 2018/06/13 09:51:51 by cpireyre         ###   ########.fr       */
+/*   Updated: 2018/06/13 12:36:59 by cpireyre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,20 @@
 
 int		main(void)
 {
-	ft_putendl("Testing return values for basic strings...");
-	ft_assert(ft_printf("1") == printf("1"));
-	ft_assert(ft_printf("") == printf(""));
-	ft_assert(ft_printf("fuck\n") == printf("fuck\n"));
-
-	ft_putendl("\n\x1b[0mTesting %%...");
-	ft_assert(ft_printf("%%o\n", 123456) == printf("%%o\n", 123456));
-	ft_assert(ft_printf("%%%%o\n", 123456) == printf("%%%%o\n", 123456));
+	ft_putendl("Testing %d...");
+	ft_printf("%d\n", 4);
+	ft_printf("with precision, no 0: %0.7d\n", 4);
+	ft_printf("without precision, yes 0: %0d\n", 4);
+	ft_printf("with dash, no 0:%0++-  #10i\n", 4);
+	ft_printf("without dash, yes 0: %0++  #10i\n", 4);
+//
+//	ft_putendl("Testing return values for basic strings...");
+//	ft_assert(ft_printf("1") == printf("1"));
+//	ft_assert(ft_printf("") == printf(""));
+//	ft_assert(ft_printf("fuck\n") == printf("fuck\n"));
+//
+//	ft_putendl("\n\x1b[0mTesting %%...");
+//	ft_assert(ft_printf("%%o\n", 123456) == printf("%%o\n", 123456));
+//	ft_assert(ft_printf("%%%%o\n", 123456) == printf("%%%%o\n", 123456));
 	return (0);
 }
